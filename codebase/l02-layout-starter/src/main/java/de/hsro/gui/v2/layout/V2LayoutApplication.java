d3bbd48430a3646cfb87083f7b5e43f3a94fa824
Veronika Dashuber <veronika.dashuber@qaware.de>
2024-03-25 10:04:11 +0100

    02 - layouts - add demo starter

---
+++ codebase/l02-layout-starter/src/main/java/de/hsro/gui/v2/layout/V2LayoutApplication.java
@@ -0,0 +1,48 @@
+/*
+ * _____________________________________________________________________________
+ * 
+ *           Project:    GUI 2015
+ * _____________________________________________________________________________
+ * 
+ *        Created by:    Johannes Weigend, QAware GmbH
+ *     Creation date:    March - July 2015
+ * _____________________________________________________________________________
+ * 
+ *         Copyright:    Hochschule Rosenheim
+ * _____________________________________________________________________________ 
+ */
+package de.hsro.gui.v2.layout;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author weigend
+ */
+public class V2LayoutApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        HBox root = new HBox();
+        root.getChildren().add(new TextField("Suchbegriff eingeben"));
+        root.getChildren().add(new Button("Los!"));
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Layouts");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
